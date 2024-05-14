a3db3154ea5a7bd4eda986c9e61f44c0b336f725
stark-hash <91049862+stark-hash@users.noreply.github.com>
2024-05-14 08:15:16 +0530

    Update forward.py

--- plugins/forward.py
+++ plugins/forward.py
@@ -37,7 +37,7 @@ async def forward(bot, query):
         CANCEL[query.from_user.id] = True
 
 
-@Client.on_message((filters.forwarded | (filters.regex(r"(https://)?(t\.me/|telegram\.me/|telegram\.dog/)(c/)?(\d+|[a-zA-Z_0-9]+)/(\d+)$")) & filters.text) & filters.private & filters.incoming))
+@Client.on_message((filters.forwarded | (filters.regex(r"(https://)?(t\.me/|telegram\.me/|telegram\.dog/)(c/)?(\d+|[a-zA-Z_0-9]+)/(\d+)$")) & filters.text) & filters.private & filters.incoming)
 async def send_for_forward(bot, message):
     if message.text:
         regex = re.compile(r"(https://)?(t\.me/|telegram\.me/|telegram\.dog/)(c/)?(\d+|[a-zA-Z_0-9]+)/(\d+)$")
