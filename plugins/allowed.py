563454cf24f68a678c85b64ec99cf8df155dbc4d
stark-hash <91049862+stark-hash@users.noreply.github.com>
2024-05-14 08:27:50 +0530

    Update allowed.py

--- plugins/allowed.py
+++ plugins/allowed.py
@@ -1,20 +1,13 @@
 from pyrogram import Client, filters
 from pyrogram.types import Message
-from info import PRIVATE_BOT, OWNER, UPDATES_CHANNEL, DEV_LINK
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from info import UPDATES_CHANNEL, DEV_LINK
 
-
-async def owner(_, client, message: Message):
-    if PRIVATE_BOT:
-        return message.from_user.id != OWNER
-
-allowed_user = filters.create(owner)
-
-@Client.on_message(filters.private & allowed_user & filters.incoming)
-async def not_owner(bot, message):
+@Client.on_message(filters.private & filters.incoming)
+async def handle_message(bot, message: Message):
     btn = [[
         InlineKeyboardButton('Updates Channel', url=UPDATES_CHANNEL)
     ],[
         InlineKeyboardButton('Contact', url=DEV_LINK)
     ]]
-    await message.reply("You can't access this bot, Create your own bot.", reply_markup=InlineKeyboardMarkup(btn))
+    await message.reply("Welcome to the bot! Feel free to explore the options below.", reply_markup=InlineKeyboardMarkup(btn)) 
