821b088b2685ea15cacb837e16ea9fed6d1e0cb6
stark-hash <91049862+stark-hash@users.noreply.github.com>
2024-05-14 06:38:59 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,13 +1,13 @@
 from os import environ
 
-API_ID = int(environ.get("API_ID", ""))
-API_HASH = environ.get("API_HASH", "")
-BOT_TOKEN = environ.get("BOT_TOKEN", "")
-FILE_CAPTION = environ.get("FILE_CAPTION", "<code>{file_name}</code>")
-OWNER = environ.get("OWNER", "")
+API_ID = int(environ.get("API_ID", "18329555"))
+API_HASH = environ.get("API_HASH", "7bf83fddf8244fddfb270701e31470a8")
+BOT_TOKEN = environ.get("BOT_TOKEN", "7052097306:AAGtz7eZ1LFZzOJPOzSXtERURu7FEblX7wA")
+FILE_CAPTION = environ.get("FILE_CAPTION", "<code>{file_name}</code> Join @StarkBotUpdates")
+OWNER = environ.get("OWNER", "1195233863")
 PRIVATE_BOT = environ.get("PRIVATE_BOT", False)
-UPDATES_CHANNEL = environ.get("UPDATES_CHANNEL", "https://t.me/webverseupdates")
-SUPPORT_GROUP = environ.get("SUPPORT_GROUP", "https://t.me/webversegroup")
-DEV_LINK = environ.get("DEV_LINK", "https://t.me/bhaiyajixbot")
+UPDATES_CHANNEL = environ.get("UPDATES_CHANNEL", "https://t.me/StarkBotUpdates")
+SUPPORT_GROUP = environ.get("SUPPORT_GROUP", "https://t.me/StarkBotSupport")
+DEV_LINK = environ.get("DEV_LINK", "https://t.me/TGTesla")
 
 # customise as per your need
